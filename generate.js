b115f9a5983ae9b167c80b29cd1452963fdfba01
Gabriel Plassard <gabriel.plassard@datadoghq.com>
2024-03-01 14:33:03 +0100

    compact + not random

--- generate.js
+++ generate.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const lengths = [25_000, 100_000, 250_000, 1_000_000, 2_000_000, 2_500_000];
+const lengths = [25_000, 100_000, 250_000, 1_000_000, 1_700_000, 2_500_000];
 for (const length of lengths) {
     const values = Array.from(Array(length).keys())
         .map((idx) => 10_000 + (idx % 90_000));
