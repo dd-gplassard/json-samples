e5a353339cfad0de488c09a31d1227225d95c4d1
Gabriel Plassard <gabriel.plassard@datadoghq.com>
2024-03-05 18:35:20 +0100

    add huge numbers

--- generate.js
+++ generate.js
@@ -1,17 +1,19 @@
 const fs = require('fs');
 
 
-const lengths = [25_000, 100_000, 250_000, 900_000,1_000_000, 1_700_000, 2_500_000];
+const lengths = [25_000, 100_000, 250_000, 700_000,900_000,1_000_000, 1_700_000, 2_500_000];
 for (const length of lengths) {
     const values = Array.from(Array(length).keys())
         .map((idx) => 10_000 + (idx % 90_000));
-
 
     const formatted = new Intl.NumberFormat('en-US').format(length).replace(/,/g, '-');
     fs.writeFileSync(`${formatted}-json-integers.json`, JSON.stringify({values}, undefined, 2));
     fs.writeFileSync(`${formatted}-compact-json-integers.json`, JSON.stringify({values}, undefined, 0));
     const doubleValues = values.map((value) => value + 0.003);
     fs.writeFileSync(`${formatted}-compact-json-doubles.json`, JSON.stringify({doubleValues}, undefined, 0));
+
+    const hugeValues = values.map((value) => value + 1_708_563_600_000.0);
+    fs.writeFileSync(`${formatted}-compact-json-huge.json`, JSON.stringify({hugeValues}, undefined, 0));
 
     const chunked = [];
     const chunkSize = 2;
